86f52b03fbd2ee3e785f8451fd27a4a9f1eb6fc6
SimoneSantacroce <simonesantacroce@tiscali.it>
2017-04-14 11:26:29 +0200

    Update TwitterListener.py
    Removing of the 1-minute delta for the collecting tweets phase

--- Collector/TwitterListener.py
+++ Collector/TwitterListener.py
@@ -14,18 +14,12 @@ class TwitterListener(StreamListener):
 	def __init__(self):
 		super(TwitterListener, self).__init__()
 		self._expire = datetime.now() + timedelta(hours=6)
-		self._lastdate = datetime.strptime("0001-01-01 00:00:00.000000", "%Y-%m-%d %H:%M:%S.%f")
 
 	def on_status(self, status):
 		current_date = datetime.now()
 
 		if current_date >= self._expire:
 			return False # break the stream
-
-		if (current_date-self._lastdate) <= timedelta(minutes=1):
-			return True
-
-		self._lastdate = current_date
 
 		try:
 			txt = status.text
